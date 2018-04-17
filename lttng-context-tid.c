7ea08ff2cf2ed121347e6a887dce43897397a888
Harshal Sheth <hsheth2@gmail.com>
2018-04-17 17:07:30 -0400

    Add jaeger fields to context-tid

--- lttng-context-tid.c
+++ lttng-context-tid.c
@@ -28,13 +28,28 @@
 #include <wrapper/vmalloc.h>
 #include <lttng-tracer.h>
 
+struct _jaeger_data_packed {
+	uint64_t jaeger_trace_id;
+	uint64_t jaeger_parent_id;
+	uint64_t jaeger_span_id;
+	char pad;
+	pid_t tid;
+	char padd;
+};
+
+union jaeger_data_packed {
+	struct _jaeger_data_packed data;
+	char as_char[sizeof(struct _jaeger_data_packed)];
+};
+
+#define LTTNG_JAEGER_CTX_LEN (sizeof(struct jaeger_data_packed))
+
 static
 size_t tid_get_size(size_t offset)
 {
 	size_t size = 0;
 
-	size += lib_ring_buffer_align(offset, lttng_alignof(pid_t));
-	size += sizeof(pid_t);
+	size += LTTNG_JAEGER_CTX_LEN;
 	return size;
 }
 
@@ -43,11 +58,17 @@ void tid_record(struct lttng_ctx_field *field,
 		 struct lib_ring_buffer_ctx *ctx,
 		 struct lttng_channel *chan)
 {
-	pid_t tid;
+	struct jaeger_data_packed store;
 
-	tid = task_pid_nr(current);
-	lib_ring_buffer_align_ctx(ctx, lttng_alignof(tid));
-	chan->ops->event_write(ctx, &tid, sizeof(tid));
+	store.data.tid = task_pid_nr(current);
+	store.data.jaeger_trace_id = current->jaeger_trace_id;
+	store.data.jaeger_parent_id = current->jaeger_parent_id;
+	store.data.jaeger_span_id = current->jaeger_span_id;
+
+	store.data.pad = 0;
+	store.data.padd = 0;
+
+	chan->ops->event_write(ctx, store.as_char, LTTNG_JAEGER_CTX_LEN);
 }
 
 static
@@ -55,10 +76,7 @@ void tid_get_value(struct lttng_ctx_field *field,
 		struct lttng_probe_ctx *lttng_probe_ctx,
 		union lttng_ctx_value *value)
 {
-	pid_t tid;
-
-	tid = task_pid_nr(current);
-	value->s64 = tid;
+	value->str = (char *)(&current->jaeger_trace_id);
 }
 
 int lttng_add_tid_to_ctx(struct lttng_ctx **ctx)
@@ -73,13 +91,16 @@ int lttng_add_tid_to_ctx(struct lttng_ctx **ctx)
 		return -EEXIST;
 	}
 	field->event_field.name = "tid";
-	field->event_field.type.atype = atype_integer;
-	field->event_field.type.u.basic.integer.size = sizeof(pid_t) * CHAR_BIT;
-	field->event_field.type.u.basic.integer.alignment = lttng_alignof(pid_t) * CHAR_BIT;
-	field->event_field.type.u.basic.integer.signedness = lttng_is_signed_type(pid_t);
-	field->event_field.type.u.basic.integer.reverse_byte_order = 0;
-	field->event_field.type.u.basic.integer.base = 10;
-	field->event_field.type.u.basic.integer.encoding = lttng_encode_none;
+	field->event_field.type.atype = atype_array;
+	field->event_field.type.u.array.elem_type.atype = atype_integer;
+	field->event_field.type.u.array.elem_type.u.basic.integer.size = sizeof(char) * CHAR_BIT;
+	field->event_field.type.u.array.elem_type.u.basic.integer.alignment = lttng_alignof(char) * CHAR_BIT;
+	field->event_field.type.u.array.elem_type.u.basic.integer.signedness = lttng_is_signed_type(char);
+	field->event_field.type.u.array.elem_type.u.basic.integer.reverse_byte_order = 0;
+	field->event_field.type.u.array.elem_type.u.basic.integer.base = 10;
+	field->event_field.type.u.array.elem_type.u.basic.integer.encoding = lttng_encode_none;
+	field->event_field.type.u.array.length = LTTNG_JAEGER_CTX_LEN;
+
 	field->get_size = tid_get_size;
 	field->record = tid_record;
 	field->get_value = tid_get_value;
