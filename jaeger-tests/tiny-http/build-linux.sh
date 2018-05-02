bdf7f942c0e70986eb309477117c1e65bba14be3
Andrew Sun <me@andrewsun.com>
2018-05-02 18:53:57 -0400

    jaaaaaaeeeeger

--- jaeger-tests/tiny-http/build-linux.sh
+++ jaeger-tests/tiny-http/build-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clang++ -std=c++11 -lopentracing -ljaegertracing -lssl -lz -lcrypto -luv -luWS /root/jaeger-client-cpp-0.3.0/build/libjaegertracing.a test.cpp
+g++ -O3 -march=native -flto -std=c++11 -lopentracing -ljaegertracing -lssl -lz -lcrypto -luv -luWS /root/jaeger-client-cpp-0.3.0/build/libjaegertracing.a test.cpp
--- jaeger-tests/tiny-http/test.cpp
+++ jaeger-tests/tiny-http/test.cpp
@@ -13,12 +13,15 @@ using namespace opentracing;
 
 using namespace uWS;
 
-void inject_jaeger(uint64_t jaeger_trace_id, uint64_t jaeger_parent_id) {
-  printf("jaeger_trace_id: %llu, jaeger_parent_id: %llu\n", jaeger_trace_id,
-         jaeger_parent_id);
+void
+inject_jaeger(uint64_t jaeger_trace_id, uint64_t jaeger_parent_id)
+{
+  //printf("jaeger_trace_id: %llu, jaeger_parent_id: %llu\n",
+  //       jaeger_trace_id,
+  //       jaeger_parent_id);
 
-  FILE *procfile = fopen("/proc/lttng_jaeger", "w");
-  uint64_t *buf = (uint64_t *)malloc(sizeof(uint64_t) * 2);
+  FILE* procfile = fopen("/proc/lttng_jaeger", "w");
+  uint64_t* buf = (uint64_t*)malloc(sizeof(uint64_t) * 2);
 
   buf[0] = jaeger_trace_id;
   buf[1] = jaeger_parent_id;
@@ -27,34 +30,43 @@ void inject_jaeger(uint64_t jaeger_trace_id, uint64_t jaeger_parent_id) {
   fclose(procfile);
 }
 
-int main() {
+int
+main()
+{
   auto config = jaegertracing::Config(
-      false,
-      jaegertracing::samplers::Config(jaegertracing::kSamplerTypeConst, 1),
-      jaegertracing::reporters::Config(
-          jaegertracing::reporters::Config::kDefaultQueueSize,
-          std::chrono::seconds(1), true));
-  auto tracer = jaegertracing::Tracer::make("test", config);
-
-  auto parent_span = tracer->StartSpan("parent");
-  assert(parent_span);
+    false,
+    jaegertracing::samplers::Config(jaegertracing::kSamplerTypeConst, 1),
+    jaegertracing::reporters::Config(
+      jaegertracing::reporters::Config::kDefaultQueueSize,
+      std::chrono::seconds(1),
+      true));
+  auto tracer = jaegertracing::Tracer::make("uws-web-cpp", config);
 
   Hub h;
   std::string response = "Hello!";
 
-  h.onHttpRequest([&](HttpResponse *res, HttpRequest req, char *data,
-                      size_t length, size_t remainingBytes) {
-    std::cout << length << std::endl;
-    auto parent_span = tracer->StartSpan("parent");
-    assert(parent_span);
-    parent_span->Finish();
+  h.onHttpRequest([&](HttpResponse* res,
+                      HttpRequest req,
+                      char* data,
+                      size_t length,
+                      size_t remainingBytes) {
+    // std::cout << length << std::endl;
+    std::unique_ptr<opentracing::v1::Span> _parent_span =
+      tracer->StartSpan("request");
+    jaegertracing::Span* parent_span = (jaegertracing::Span*) _parent_span.get();
+    jaegertracing::SpanContext ctx = parent_span->contextNoLock();
+    uint64_t trace_id = ctx.traceID().low();
+    uint64_t span_id = ctx.spanID();
+    inject_jaeger(trace_id, span_id);
 
-    parent_span->Log({{"event", "simple log"}, {"abc", 123}});
+    // std::cout << trace_id << ", " << span_id << std::endl;
+    // assert(parent_span);
+    // parent_span->Finish();
+
+//    parent_span->Log({ { "event", "simple log" }, { "abc", 123 } });
 
     res->end(response.data(), response.length());
     parent_span->Finish();
-    tracer->Close();
-    // std::cout << oss.str() << "\n";
   });
 
   if (h.listen(3000)) {
