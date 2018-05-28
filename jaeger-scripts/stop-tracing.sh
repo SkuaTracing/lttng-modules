6affb31443c965bb99edeb93f7203c4f66e816ad
skuabot <andy.sun00+skuabot@gmail.com>
2018-05-28 13:40:27 -0400

    Moved jaeger-ctx, jaeger-scripts, and jaeger-tests to different repos

--- jaeger-scripts/stop-tracing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-lttng stop
-sleep 10
-lttng destroy
-sleep 3
-killall babeltrace
-sleep 0.1
-
--- jaeger-tests/tiny-http/build-linux-nothing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -O3 -march=native -flto -std=c++11 -lssl -lz -lcrypto -luv -luWS -DNO_JAEGER test.cpp
